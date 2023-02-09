080bd611b8edbf54ab29fe33d05090d25016ba8a
周湛 <>
2023-02-09 18:15:33 +0800

    ..

--- src/Swiper.js
+++ src/Swiper.js
@@ -6,53 +6,75 @@ import kimage from "./img/kubosan.webp"
 import jQuery from 'jquery'
 const Swiper = () => {
     let [items, setItem] = useState([])
-    let [containerStyle, setStyle ]= useState({ justifyContent: "flex-start" })
-    const [delayHandler, setDelayHandler] = useState(null)
-    console.log("render")
+    let [containerStyle, setStyle] = useState({ justifyContent: "flex-start" })
+    let [scaleStack, setScaleStack] = useState({})
+    const timeout = 300
     useEffect(() => {
         setItem((item) => [1, 2, 3, 4, 5, 6])
-       
+
     }, [])
 
     const onMouseOver = (e) => {
         let a = e.currentTarget
-        
-        let id = e.currentTarget.dataset.id
-        if(parseInt(id) > 3) {
-            setStyle((state) => {
-                // state.justifyContent = 'flex-end'
-                state = {
-                    ...state,
-                    justifyContent: 'flex-end'
-                }
-                return state
-            })
-            
-        } else {
-            setStyle((state) => {
-                // state.justifyContent = 'flex-end'
-                state = {
-                    ...state,
-                    justifyContent: 'flex-start'
-                }
-                return state
-            })
-        }
-        // setDelayHandler(setTimeout(() => {
-            if(!a.classList.contains('is-hover-active-play')) {
-                // setTimeout(() => {
-                    a.classList.add("is-hover-active-play")
-                // },200)
-            }
-        // }, 300))
-        
 
-       
+        let id = e.currentTarget.dataset.id
+        
+        // setDelayHandler(setTimeout(() => {
+        if (!a.classList.contains('is-hover-active-play')) {
+            let start = performance.now()
+            
+            setScaleStack((items) => {
+                return {
+                    ...items,
+                    [id]: {
+                        t: start,
+                        c: setTimeout(() => {
+                            a.classList.add("is-hover-active-play")
+
+                            if (parseInt(id) > 4) {
+                                setStyle((state) => {
+                                    // state.justifyContent = 'flex-end'
+                                    state = {
+                                        ...state,
+                                        justifyContent: 'flex-end'
+                                    }
+                                    return state
+                                })
+                    
+                            } else {
+                                setStyle((state) => {
+                                    // state.justifyContent = 'flex-end'
+                                    state = {
+                                        ...state,
+                                        justifyContent: 'flex-start'
+                                    }
+                                    return state
+                                })
+                            }
+                            // console.log(performance.now() - start, "____")
+                        }, timeout)
+                    }
+                }
+            })
+
+
+        }
+        // }, 300))
+
+
+
 
     }
     const onMouseLeave = (e) => {
         let a = e.currentTarget
+        let id = e.currentTarget.dataset.id
         a.classList.remove("is-hover-active-play")
+        // console.log(performance.now() - scaleStack[id].t)
+        console.log(scaleStack)
+        if(performance.now() - scaleStack[id].t < timeout) {
+            clearTimeout(scaleStack[id].c)
+        }
+        
         // clearTimeout(delayHandler)
     }
     return <>
@@ -69,7 +91,7 @@ const Swiper = () => {
                                     <img src={kimage}></img>
                                 </div>
                                 <div className="ranking-cell-desc">
-                                    <div className="ranking-cell-index" style={{ color: "rgba(234,62,205,0.298)" }}>{v+1}</div>
+                                    <div className="ranking-cell-index" style={{ color: "rgba(234,62,205,0.298)" }}>{v + 1}</div>
                                     <div className="hotranking-content-item-desc">
                                         <a><div className="home-cell-desc-title">间谍过家家</div></a>
 
